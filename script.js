9522dcdac0b01da1a9dd0f4451a3935e438f06e1
Harshadeep6 <harshadeepmore@gmail.com>
2024-01-24 17:12:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+const cl = console.log.bind()
+
+const colorPallete = [
+    "red", // Gajar ka rang
+    "green", // Patta hara
+    "yellow", // Aasman ka rang
+    "violet", // Gulabi mehfil
+    "indigo", // Suraj ki kirne
+    "orange", // Baingani shaan
+    "aqua", // Sona chandi
+    "crimson", // Neela aasman
+    "khaki", // Tamatar laal
+    "lavender", // Aasmani paani
+    "blue", // Kesar bhara orange
+    "white"  // Badal ka bhatakti saaya
+];
+
+selectedColor = colorPallete[0]
+
+const boxes = document.querySelectorAll('.boxes')
+
+let isMousePressed = false;
+
+boxes.forEach(element => {
+    element.addEventListener('mousedown', startColoring)
+    element.addEventListener('mouseover', colorOnHover)
+});
+
+function startColoring(event) {
+    isMousePressed = true;
+    colorBox(event.target.id)
+}
+
+function colorOnHover(event) {
+    if(isMousePressed) colorBox(event.target.id)
+}
+
+function colorBox(id) {
+    const box = document.getElementById(id)
+    box.style.backgroundColor = selectedColor;
+}
+
+document.addEventListener('mouseup', () => isMousePressed = false)
+
+const password = document.querySelector('#password')
+const gridContainer = document.querySelector('.gridContainer')
+const button = document.querySelector('button')
+const colors = document.querySelectorAll('.colors')
+
+password.addEventListener('focus', () => {
+    password.classList.toggle('focus')
+    gridContainer.classList.toggle('opa')
+    button.classList.toggle('buttonVisible')
+})
+
+
+colors.forEach((color, idx) => {
+    color.addEventListener('click', () => {
+        selectedColor = colorPallete[idx]
+    })
+})
